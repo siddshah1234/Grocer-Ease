33c684525362814faca2a69a922dc044e2218c30
siddshah1234 <151389521+siddshah1234@users.noreply.github.com>
2024-10-20 22:34:51 -0400

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {"message": "Hello from Flask!"}
-    return jsonify(data)
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
